740baf87c61d789cb213f34c82561ad48f4c42e6
Unknown <spartakus1688@gmail.com>
2018-09-12 21:45:28 +0300

    Task-3 ready

---
+++ task-3.rb
@@ -0,0 +1,9 @@
+array_fibonacci = [0,1]
+
+loop do
+  next_number = array_fibonacci[-1] + array_fibonacci[-2]
+  break if next_number >= 100
+  array_fibonacci  << next_number
+end
+
+print array_fibonacci
